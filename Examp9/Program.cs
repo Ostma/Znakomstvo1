5c8433184d078ecb6b3800124ba854e56301ad79
MaxO <ostma@mail.ru>
2022-10-02 22:18:58 +0500

    Primer9

---
+++ Examp9/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {11, 12, 325, 346, 18, 64, 23, 53, 33};
+
+int n = array.Length;
+int find = 64;
+int index = 0;
+while(index<n){
+    if (array[index]== find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
