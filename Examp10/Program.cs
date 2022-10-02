b343c4a47e4f8ad15e5ec73763bbe9179b6c77e1
MaxO <ostma@mail.ru>
2022-10-02 22:35:49 +0500

    Primer10

---
+++ Examp10/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] coll)
+{
+    int length = coll.Length;
+    int index = 0;
+    while (index < length)
+    {
+       coll[index] = new Random().Next(1, 10);
+       index++;
+    }
+}
+
+void PrintArray(int[] collect)
+{
+    int count = collect.Length;
+    int pos = 0;
+
+    while (pos < count)
+    {
+        Console.WriteLine(collect[pos]);
+        pos++;
+    }
+
+}
+
+int [] array = new int[10];
+FillArray(array);
+PrintArray(array);
